a600b93a1b2121b708885f82a5e54e892740b44a
Yurii Khomiak <luna.loooony.lovegood@gmail.com>
2019-03-12 23:28:11 +0200

    Added functions for creating of vector columns and rows

--- lab2/lab2/main.cpp
+++ lab2/lab2/main.cpp
@@ -2,10 +2,22 @@
 
 #include <iostream>
 
+matrix::Matrix CreateVectorColumn(std::size_t number_of_rows);
+matrix::Matrix CreateVectorRow(std::size_t number_of_columns);
+
 int main() {
   using namespace matrix;
-  std::cout << Matrix::identity(5);
+  std::cout << CreateVectorColumn(5);
+  std::cout << CreateVectorRow(5);
 
   system("pause");
   return 0;
+}
+
+matrix::Matrix CreateVectorColumn(std::size_t number_of_rows) {
+  return matrix::Matrix(number_of_rows, 1);
+}
+
+matrix::Matrix CreateVectorRow(std::size_t number_of_columns) {
+  return matrix::Matrix(1, number_of_columns);
 }
